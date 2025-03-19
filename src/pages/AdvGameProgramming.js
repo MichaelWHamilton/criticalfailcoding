474def6707e75fe512ceff4b8c56e96833fdb30c
MichaelWHamilton <mwhamilton@ualr.edu>
2025-03-18 23:08:02 -0500

    adv game programming

---
+++ src/pages/AdvGameProgramming.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const AdvGameProgramming = () => {
+    return (
+        <div>
+            <h1>Advanced Game Programming</h1>
+            <p>test</p>
+        </div>
+    );
+}
+export default AdvGameProgramming;
