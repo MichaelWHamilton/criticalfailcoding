b94e5dfe82b6175041d0ff10974514ea33200ef0
MichaelWHamilton <mwhamilton@ualr.edu>
2025-03-20 15:02:45 -0500

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import Home from './pages/Home';
 import ColorGame from './pages/ColorGame';
 import AdvGameProgramming from './pages/AdvGameProgramming';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/color-game" element={<ColorGame/>} />
         <Route path="/AdvGameProgramming" element={<AdvGameProgramming/>} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
